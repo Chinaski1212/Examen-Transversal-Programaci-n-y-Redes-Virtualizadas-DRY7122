b7afdbff5951c63195365c421f462fe9a529cd28
Chinaski1212 <cristobalescalaf@gmail.com>
2024-07-08 23:29:14 -0400

    Revert "Agregué item 1 e item 2"
    This reverts commit 0d8b0af0e1b41af9c5cef007cdc8cc263c71d69e.

--- item1-3 copy.py
+++
@@ -1,8 +0,0 @@
-print("-------------------")
-print(" Integrantes: ")
-print("-------------------")
-print("- Abraham Gallardo")
-print("- Rodrigo Sanhueza")
-print("- Cristobal Escala")
-print("\n")
-input("Presione Enter para salir.")
